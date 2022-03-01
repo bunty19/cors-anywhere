7bddf4efd3068e661233b2c4fd5f9ee0319f5ddd
BUNTY KUMAR SHARMA <quamatbanty01@gmail.com>
2022-02-28 17:31:52 -0800

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: [],
+  requireHeader: ["http://pluto.tv","https://smashystream.com"],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
